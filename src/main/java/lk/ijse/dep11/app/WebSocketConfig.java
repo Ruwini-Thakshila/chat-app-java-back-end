0e574f21842cbf7b6d00503801c9a5e059fc7293
Ruwini-Thakshila <ruwinithakshila2017@gmail.com>
2023-12-10 18:38:30 +0530

    Implement web services using Web Socket API

--- src/main/java/lk/ijse/dep11/app/WebSocketConfig.java
+++ src/main/java/lk/ijse/dep11/app/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package lk.ijse.dep11.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lk.ijse.dep11.app.wscontroller.ChatWsController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -11,6 +14,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 public class WebSocketConfig  implements WebSocketConfigurer {
 
+    @Primary
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean(){
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
     @Bean
     public ChatWsController chatWsController(){
         return new ChatWsController();
